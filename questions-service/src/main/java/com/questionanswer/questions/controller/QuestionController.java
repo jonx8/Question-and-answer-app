b6d7d32c165ed891887d7a9dde4d5d170b68f954
jonx8 <xekoro10@gmail.com>
2024-08-18 22:52:06 +0300

    feat: Set up swagger-ui

--- questions-service/src/main/java/com/questionanswer/questions/controller/QuestionController.java
+++ questions-service/src/main/java/com/questionanswer/questions/controller/QuestionController.java
@@ -6,10 +6,16 @@ import com.questionanswer.questions.controller.DTO.UpdateQuestionDTO;
 import com.questionanswer.questions.controller.DTO.UpdateStatusDTO;
 import com.questionanswer.questions.entity.Question;
 import com.questionanswer.questions.service.QuestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -27,17 +33,42 @@ import java.util.UUID;
 public class QuestionController {
     private final QuestionService questionService;
 
+
     @GetMapping
+    @Operation(summary = "Get questions list", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = Question.class)))
+            )}
+    )
     public List<Question> getQuestions() {
         return questionService.getQuestions();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get question by id", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Question.class))),
+            @ApiResponse(responseCode = "404", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            ))
+    })
     public Question getQuestion(@PathVariable UUID id) {
         return questionService.getQuestion(id);
     }
 
     @PostMapping
+    @Operation(summary = "Create new question", responses = {
+            @ApiResponse(responseCode = "201", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Question.class))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            ))
+    })
     public ResponseEntity<Question> createQuestion(@Valid @RequestBody CreateQuestionDTO dto,
                                                    UriComponentsBuilder uriComponentsBuilder) {
         Question question = questionService.createQuestion(dto);
@@ -50,18 +81,44 @@ public class QuestionController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Get question by id", responses = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = Question.class))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            )),
+            @ApiResponse(responseCode = "404", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            ))
+    })
     public Question updateQuestion(@PathVariable UUID id,
                                    @Valid @RequestBody UpdateQuestionDTO dto) {
         return questionService.updateQuestion(id, dto);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Change question status", responses = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            )),
+            @ApiResponse(responseCode = "404", content = @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = ProblemDetail.class)
+            ))
+    })
     public ResponseEntity<Void> changeQuestionStatus(@PathVariable UUID id,
                                                      @Valid @RequestBody UpdateStatusDTO dto) {
         questionService.changeStatus(id, dto.status());
         return ResponseEntity.noContent().build();
     }
 
+
+    @Operation(summary = "Delete question", responses = {@ApiResponse(responseCode = "204")})
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteQuestion(@PathVariable UUID id) {
         questionService.deleteQuestion(id);
