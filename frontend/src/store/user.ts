07bfa8cfb6c8ffc86ba7ce953053ada167e74adf
jonx8 <xekoro10@gmail.com>
2024-09-03 21:30:13 +0300

    refactor: Delete unused action from userStore

--- frontend/src/store/user.ts
+++ frontend/src/store/user.ts
@@ -1,17 +1,8 @@
 import {defineStore} from "pinia";
-import api from "@/api";
 import {UserRepresentation} from "@/api/generated/users";
 
 export const useUserStore = defineStore("user", {
     state: () => ({userProfile: {} as UserRepresentation}),
-    actions: {
-      async loadUserData() {
-        if (!this.userProfile.id) {
-          throw new Error("User id is undefined!")
-        }
-        Object.assign(this.userProfile, await api.usersApi.getUserInfo(this.userProfile.id))
-      }
-    }
   }
 )
 
