7b7aca6b1183f07974f18ac88f5329800862ec5f
Andrei Malykh <andrei.malykh.0@gmail.com>
2025-10-09 21:55:10 +0300

    test(users-service): fix contextLoad test

--- users-service/src/test/java/com/questionanswer/users/UsersServiceApplicationTests.java
+++ users-service/src/test/java/com/questionanswer/users/UsersServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.questionanswer.users;
 
+import com.questionanswer.users.config.TestingBeans;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestingBeans.class)
 class UsersServiceApplicationTests {
 
 	@Test
---
+++ users-service/src/test/java/com/questionanswer/users/config/TestingBeans.java
@@ -0,0 +1,15 @@
+package com.questionanswer.users.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+
+import static org.mockito.Mockito.mock;
+
+@TestConfiguration
+public class TestingBeans {
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return mock(JwtDecoder.class);
+    }
+}
